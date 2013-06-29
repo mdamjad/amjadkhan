5fb23d5bdd835e6c90d50d8b1a49f41369a38d43
mdamjad <rocky147khan@gmail.com>
2013-06-30 02:02:50 +0530

    del demo

--- demo/db/migrate/20130629201711_add_last_name_change_username_to_first_name.rb
+++
@@ -1,6 +0,0 @@
-class AddLastNameChangeUsernameToFirstName < ActiveRecord::Migration
-  def change
-    add_column :users, :last_name, :string
-    rename_column :users, :username, :first_name 
-  end
-end
