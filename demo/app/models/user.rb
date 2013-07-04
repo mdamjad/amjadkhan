a1ba5f3469e38d9ecbc036b1b443455fe6f59c10
mdamjad <rocky147khan@gmail.com>
2013-07-05 02:19:13 +0530

    demo update

--- demo/app/models/user.rb
+++ demo/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  validates :first_name,:last_name,:username, presence: true
+  validates :password, length: { in: 6..40 }
+
+  before_validation :ensure_username_has_a_value
+  protected
+  def ensure_username_has_a_value
+    if username.nil?
+      self.username = first_name.downcase+"_"+last_name.downcase
+    end
+  end
 end
--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130629201711) do
+ActiveRecord::Schema.define(version: 20130704185113) do
+
+  create_table "posts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -21,6 +26,8 @@ ActiveRecord::Schema.define(version: 20130629201711) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "last_name"
+    t.string   "password"
+    t.string   "username"
   end
 
 end
